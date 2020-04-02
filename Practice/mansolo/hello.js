9b8c470734250367ad65844fcf639f58813e563c
ethnicpoop <githuigituku@gmail.com>
2020-04-02 16:00:01 +0300

    Added lines to hello

--- Practice/mansolo/hello.js
+++ Practice/mansolo/hello.js
@@ -1,2 +1,12 @@
+var time = 0;
 console.log('Hello world');
 console.log(2*2);
+
+setTimeout(function(){
+  console.log('3seconds after');
+} ,3000);
+
+setInterval(function(){
+  time += 2;
+  console.log(time + 'seconds have passed');
+} ,2000);
